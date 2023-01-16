b5ad08fab7035e2a80123c1446bfb6177139916c
Tanaxeri <tamasgamer@gmail.hu>
2023-01-16 16:10:28 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DinamikWinKep
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TaroloPan = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // TaroloPan
+            // 
+            this.TaroloPan.Dock = System.Windows.Forms.DockStyle.Right;
+            this.TaroloPan.Location = new System.Drawing.Point(134, 0);
+            this.TaroloPan.Name = "TaroloPan";
+            this.TaroloPan.Size = new System.Drawing.Size(1066, 658);
+            this.TaroloPan.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 658);
+            this.Controls.Add(this.TaroloPan);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TaroloPan;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinamikWinKep
+{
+
+    public partial class MainForm : Form
+    {
+
+        int SorSzam = 4;
+        int OszlopSzam = 4;
+        int InsertSize = 60;
+        List<Image> images = new List<Image>();
+        Random random = new Random();
+
+        public MainForm()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            images.Add(Image.FromFile("majom.jpg"));
+            images.Add(Image.FromFile("majom2.jpg"));
+            images.Add(Image.FromFile("majom3.jfif"));
+
+            Button button = new Button();
+            button.SetBounds(280, 280, 60, 60);
+            button.Text = "Kep";
+            button.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button.AutoSize = true;
+            TaroloPan.Controls.Add(button);
+
+            for (int i = 0; i < SorSzam; i++)
+            {
+
+                for (int j = 0; j < OszlopSzam; j++)
+                {
+
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.Image = images[random.Next(images.Count)];
+                    pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBox.SetBounds(j * InsertSize, i * InsertSize, InsertSize, InsertSize);
+                    pictureBox.Click += (o, ev) =>
+                    {
+
+                        PictureBox picture = (PictureBox)o;
+                        button.Image = picture.Image;
+
+                    };
+                    TaroloPan.Controls.Add(pictureBox);
+
+                }
+
+            }
+
+
+        }
+
+    }
+
+}
